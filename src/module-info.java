cefc346b94dbf08c0b667e0e4ec4cdfaeb999ef4
PKopel <pawel.kopel2@gmail.com>
2020-05-16 13:27:43 +0200

    fixed javafx issues

---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module JCook {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens sample;
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,26 +1,18 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn1=new Button("Say, Hello World");
-        btn1.setOnAction(arg0 -> {
-            System.out.println("hello world");
-        });
-        StackPane root=new StackPane();
-        root.getChildren().add(btn1);
-        Scene scene=new Scene(root,600,400);
-        primaryStage.setTitle("First JavaFX Application");
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
