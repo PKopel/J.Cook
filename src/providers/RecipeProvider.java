b19e27f42ae27eac8d8c4bb311566af394810e87
PKopel <pawel.kopel2@gmail.com>
2020-05-20 15:33:13 +0200

    alternative version of RecipeProvider

--- src/providers/RecipeProvider.java
+++ src/providers/RecipeProvider.java
@@ -9,6 +9,7 @@ import models.Ingredient;
 import models.Rating;
 import models.Recipe;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import java.util.Collection;
@@ -24,60 +25,39 @@ public class RecipeProvider {
         MongoClient mongoClient = new MongoClient();
         db = mongoClient.getDatabase(databaseName);
     }
-public Collection<Recipe> getAllRecipes(RecipeFilter filter) {
-        Collection<Recipe> res = new LinkedList<>();
 
-        db.getCollection("recipe")
+    private Recipe readRecipe(Document doc){
+        Collection<Ingredient> ingredients = doc.getList("ingredients", Ingredient.class);
+        Collection<ObjectId> ratingIds = doc.getList("ratings", ObjectId.class);
+        Collection<String> tags = doc.getList("tags", String.class);
+        Collection<Category> categories =
+                doc.getList("categories", String.class)
+                        .stream().map(Category::getCategory)
+                        .collect(Collectors.toList());
+        return new Recipe(
+                doc.getObjectId("_id"),
+                doc.getString("name"),
+                doc.getString("image"),
+                ingredients,
+                ratingIds,
+                tags,
+                categories);
+    }
+
+    public Collection<Recipe> getRecipes(RecipeFilter filter){
+        return db.getCollection("recipe")
                 .find(filter.getQuery())
-                .into(new LinkedList<>())
-                .forEach((object) -> {
-                    // TODO: not yet sure if collections creation works
-                    Collection<Ingredient> ingredients = new LinkedList<>();
-                    ((List<Document>) object.get("ingredients")).forEach((ingredient) -> {
-                        ingredients.add(new Ingredient(ingredient.getString("name"), ingredient.getInteger("quantity"), ingredient.getString("unit")));
-                    });
-                    Collection<ObjectId> ratingIds = new LinkedList<>();
-                    ((List<Document>) object.get("ratings")).forEach((rating) -> {
-                        ratingIds.add(rating.getObjectId("rating_id"));
-                    });
-                    Collection<String> tags = new LinkedList<>();
-                    ((List<Document>) object.get("tags")).forEach((tag) -> {
-                        tags.add(tag.getString("tag"));
-                    });
-                    Collection<Category> categories = new LinkedList<>();
-                    ((List<Document>) object.get("categories")).forEach((category) -> {
-                        categories.add(Category.getCategory(category.getString("category")));
-                    });
-
-                    res.add(new Recipe(
-                            object.getObjectId("_id"),
-                            object.getString("name"),
-                            object.getString("image"),
-                            ingredients,
-                            ratingIds,
-                            tags,
-                            categories
-                            ));
-                });
-
-        return res;
+                .map(this::readRecipe)
+                .into(new LinkedList<>());
     }
 
     public Collection<Rating> getRatings(Collection<ObjectId> ids) {
-        Collection<Rating> res = new LinkedList<>();
 
-        ids.forEach((id) -> db.getCollection("ratings")
-                .find(Filters.eq("_id", id))
-                .into(new LinkedList<>())
-                .forEach((object) -> {
-                    res.add(new Rating(
-                            id,
-                            object.getInteger("stars"),
-                            object.getString("description"),
-                            object.getDate("date")
-                    ));
-                }));
-
-        return res;
+        Collection<Bson> idsQuery = ids.stream()
+                .map(id -> Filters.eq("_id", id))
+                .collect(Collectors.toList());
+        return db.getCollection("ratings")
+                .find(Filters.or(idsQuery), Rating.class)
+                .into(new LinkedList<>());
     }
 }
