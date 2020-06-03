3eb6832e8543244cf3a76a4356dee27972c66d43
PKopel <pawel.kopel2@gmail.com>
2020-06-03 10:38:05 +0200

    add test

--- src/providers/RecipeProvider.java
+++ src/providers/RecipeProvider.java
@@ -7,4 +7,5 @@ public class RecipeProvider extends AbstractProvider<Recipe> {
     public RecipeProvider(String databaseName) {
         super(databaseName, "recipe", Recipe.class);
     }
+
 }
---
+++ test/providers/RecipeProviderTest.java
@@ -0,0 +1,17 @@
+package providers;
+
+import models.Recipe;
+import org.junit.jupiter.api.Test;
+
+public class RecipeProviderTest {
+    @Test
+    public void addObjectTest(){
+        RecipeProvider recipeProvider = new RecipeProvider("JCookTest");
+        //Recipe recipe = mock(Recipe.class);
+    }
+
+    @Test
+    public void getObjectsTest(){
+
+    }
+}
